c5b8e54f1ea9d096aca8cd027eea62ac14ab8d12
barlehmann <bar.lehmann@gmail.com>
2020-04-22 18:04:30 -0400

    adding further comments to pearson and scatterplot function to ensure good readability by user

--- Pearson_and_Scatterplt_Function.py
+++ Pearson_and_Scatterplt_Function.py
@@ -31,53 +31,46 @@ MFT = pd.read_excel('Mood_Focus_Table.xlsx')
 
 
 # Defining variables/columns for exploration of possible trends
-GSR_Values = MFT['GSR_Values']
-Mood_self_rating = MFT['Mood_self_rating']
-Focus_self_rating = MFT['Focus_self_rating']
-Date_Time = MFT['Date_Time']
-Average_Total_Amplitude = MFT['Average_Total_Amplitude']
-Alpha_Amplitude = MFT['Alpha_Amplitude']
+GSR = MFT['GSR_Values']
+Mood = MFT['Mood_self_rating']
+Focus = MFT['Focus_self_rating']
+Time = MFT['Date_Time']
+Avg_tot_amp = MFT['Average_Total_Amplitude']
+Alpha_amp = MFT['Alpha_Amplitude']
 
 
 #Creating Mood_Focus_Table customized (set by user) analysis of Pearson R and R^2
 
-print("Below are the columns/data from which you may choose two to perform a pearson correlation analysis and scatterplot on. Choose two columns to begin and watch your spelling!")
+#First, print an introductory prompt for user to input the columns that wishes to do pearson r and scatterplot
+# analyses on
+print("Below are the columns/data from which you may choose two to perform a pearson correlation analysis and scatterplot on. Choose two columns to begin and watch your spe")
 print(MFT.columns)
 
+# Next, prompting user to input their variables of choice
 Chosen_X = globals()[input("Type in your first (or x-axis) variable: ")]
 Chosen_y = globals()[input("Type in your second (or y-axis) variable: ")]
 
-#if Chosen_X == 'Mood' or Chosen_X == 'Mood':
-
-#    print("Great pick for x")
-#elif Chosen_y == 'Mood' or Chosen_y == 'Mood':
-#    print("Great pick for y")
-#else:
-#    print("Uh oh, I don't know about that item")
-
+#Outputting the pearson correlation analysis information
 print("Mood and focus self ratings have a pearson r and r^2 respectively of: " )
 print(pearsonr(Chosen_X, Chosen_y))
 #print(pearsonr(Mood, Focus))
 
-# Create a scatter plot
+# outputting the scatter plot for variables that user input
 plt.scatter(Chosen_X, Chosen_y)
 
-#to access variable name
+# This is a function to access variable name, to put onto scatterplot graph automatically
 def namestr(obj, namespace):
     return [name for name in namespace if namespace[name] is obj]
 
-# Set Axis Labels and Title
+# Setting Axis Labels and Title
 plt.xlabel(namestr(Chosen_X, globals()))
 plt.ylabel(namestr(Chosen_y, globals()))
 X_var_name = namestr(Chosen_X, globals())
 Y_var_name = namestr(Chosen_y, globals())
-#plottitle = concat(X_var_name + "Vs" + Y_var_name)
-#plt.xlabel('Average_Total_Amplitude [in uV]')
-#plt.ylabel('Alpha_Amplitude [in uV]')
 plt.title(str(X_var_name) + "vs" + str(Y_var_name))
-# Show and clean up again
-plt.show()
 
-# Show and clean up again
+
+# Show scatterplot and clean up
+plt.show()
 plt.clf()
 
