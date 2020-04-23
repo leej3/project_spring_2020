5cc13b6d2fb49994f053cb1ccdbceb8ec0494890
leej3 <johnleenimh@gmail.com>
2020-04-23 17:59:15 -0400

    change setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import setuptools
+from pathlib import Path
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+SCRIPTS = [str(f) for f in Path('scripts').glob('*program.py')]
+long_description = "some text"
 
 setuptools.setup(
-    name="johns_package", 
+    name="johns_package",
     version="0.0.1",
-    author="Me!!!",
+    author="Me",
     author_email="me@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/packaging_demo",
     packages=setuptools.find_packages(),
+    scripts = SCRIPTS,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
