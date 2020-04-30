4eca16844f493a9d4a7450fa52dc5d01d584555d
barlehmann <bar.lehmann@gmail.com>
2020-04-29 20:16:53 -0400

    adding test files

---
+++ eeg_gsr_wellness_tracker/test_graph.py
@@ -0,0 +1,17 @@
+import graph
+
+'''
+def extract_mean_amplitude_to_data():
+   # assert append_table.extract_mean_amplitude_to_data(Picks = 'EEG Fp2-LE') type(sumdf) is pandas.core.frame.DataFrame
+    assert append_table.extract_mean_amplitude_to_data(Picks = 'EEG Fp2-LE') type(raw) is mne.io.edf.edf.RawEDF
+    assert append_table.extract_mean_amplitude_to_data(Picks = 'EEG Fp2-LE') type("time") is numpy.int64
+'''
+
+
+
+def test_extract_mean_amplitude_to_data():
+    assert type(sumdf) is pandas.core.frame.DataFrame
+    assert type(raw) is mne.io.edf.edf.RawEDF
+    assert type("time") is numpy.int64
+
+montage='deprecated'
