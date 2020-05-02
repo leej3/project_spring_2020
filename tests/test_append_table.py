ffd87cccb3742108cc96a8189ff8568d0fd5861d
barlehmann <bar.lehmann@gmail.com>
2020-05-02 17:47:50 -0400

    updating test file

--- tests/test_append_table.py
+++ tests/test_append_table.py
@@ -3,6 +3,7 @@ from eeg_gsr_wellness_tracker import append_table
 import pandas
 import mne
 import numpy
+from pathlib import Path
 
 
 '''
@@ -14,17 +15,24 @@ def extract_mean_amplitude_to_data():
 
 
 
-
 def read_raw_eeg_file(eeg_file_path):
     assert isinstance(eeg_file_path,  str)
     assert isinstance(raw,  RawEDF)
     
     
-def test_extract_mean_amplitude_to_data():
+    
+@pytest.mark.parametrize("Picks", ["EEG Fp2-LE"])
+@pytest.mark.parametrize("excel_file", ["Mood_Focus_Table.xlsx"])
+def test_extract_mean_amplitude_to_data(Picks, excel_file):
+    #file_dir = excel_file / 'file'
+    #extract_mean_amplitude_to_data(Picks = 'EEG Fp2-LE')
     assert isinstance(Picks,  str)
     #assert isinstance(sumdf, )
     assert type("time") is str
-    #assert MeanPzdf = Pzdf.mean()
+    assert MeanPzdf != 0
+    assert MeanPzdf == Pzdf.mean()
     #assert picks==Picks
-
-
+    #assert Path(excel_file).is_file()
+    #assert isin(df.columns)
+    #assert isinstance (Picks, channel) Pick.type() == "channel
+    #assert excel_file.is_file()
