3471f2f925cba063952b878946db54302686f72e
dferhadian <60761137+dferhadian@users.noreply.github.com>
2020-05-04 09:51:11 -0400

    Update get_volumes.py

--- get_volumes/get_volumes.py
+++ get_volumes/get_volumes.py
@@ -1,6 +1,8 @@
 import pandas as pd
-cd dferhadianproject_spring_2020/project_spring_2020/
+%cd /Users/damien/Documents/Python class/Final project
 data = pd.read_csv("Data.csv")  #if UnicodeDecodeError open csv file in Sublime Text and Save with UTF-8 encoding
 data.head()
-clean_data = data[['Sample Time','Type', 'Conc.']]
+clean_data = data.copy()[['Sample Time','Type', 'Conc.']]
+clean_data.head()
+clean_data["Volume"] = 700/clean_data["Conc."]
 clean_data.head()
