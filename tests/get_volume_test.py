91e4b0f3a9f3e2dc00a310b9d7f10e34a66ae1f8
dferhadian <60761137+dferhadian@users.noreply.github.com>
2020-05-05 09:35:06 -0400

    Update get_volume_test.py

--- tests/get_volume_test.py
+++ tests/get_volume_test.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from get_volumes import core_functions
+from get_volumes import core_function
 
 
 def test_get_volumes():
-    expected = pd.DataFrame({"SampleConcentrions":[1,4,2],'volumes':[20,1,2]})
+    expected = pd.DataFrame({"Conc.":[10,10.1,12,2],'volumes':[70,69.3,57.38]})
     filename = 'data/data.csv'
     input = pd.DataFrame.read_csv(filename).head(3)
     result = core_functions.get_volumes(input)
