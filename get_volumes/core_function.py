4e6b384c644648a42d374162b84ae2bc2bfea5aa
dferhadian <60761137+dferhadian@users.noreply.github.com>
2020-05-05 09:38:49 -0400

    Update core_function.py

--- get_volumes/core_function.py
+++ get_volumes/core_function.py
@@ -1,6 +1,5 @@
 import pandas as pd
-%cd /Users/damien/Documents/Python class/Final project
-data = pd.read_csv("Data.csv")  #if UnicodeDecodeError open csv file in Sublime Text and Save with UTF-8 encoding
+data = pd.read_csv("tests/Data/Data.csv")  #if UnicodeDecodeError open csv file in Sublime Text and Save with UTF-8 encoding
 data.head()
 clean_data = data.copy()[['Sample Time','Type', 'Conc.']]
 clean_data.head()
