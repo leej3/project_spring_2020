b43d960d61e24bde512a955e82321124a15bc014
Christina Wusinich <christina.wusinich@nih.gov>
2020-05-06 15:08:51 -0400

    Updated setup file and renamed a few things

---
+++ MID_processing/__init__.py
@@ -0,0 +1,3 @@
+'''Tools for processing MID MEG and behavioral data!'''
+
+__version__='0.2.0'
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
  
+with open('README.md','r') as yeet:
+	long_description=yeet.read()
+
 setup(
-        name = 'MID_process',
+        name = 'MID_processing',
         author = 'Christina Wusinich',
+	author_email='christinawusinich@gmail.com',
         description = 'Some tools for processing MID behavioral and MEG data',
-        license = 'MIT',
-        packages = find_packages()
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/cwusinich/project_spring_2020',
+        license = 'Apache',
+        packages = find_packages(),
+	python_requires='>=3.6'
      )
 
 
