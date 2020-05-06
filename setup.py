8d377544f0e5823d078c519651529bd6fcb3879c
cwusinich <60762403+cwusinich@users.noreply.github.com>
2020-05-06 12:54:43 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-#this will have things in it soon!
+from setuptools import setup, find_packages
+ 
+setup(
+        name = 'MID_process',
+        author = 'Christina Wusinich',
+        description = 'Some tools for processing MID behavioral and MEG data',
+        license = 'MIT',
+        packages = find_packages()
+     )
+
+
