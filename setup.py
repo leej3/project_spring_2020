f80a44f72a564dde9dedba10007747dd0a1fbe07
harveycas <harveycas@nih.gov>
2020-05-05 23:03:13 -0400

    05052020

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tetrad_analysis", 
+    version="0.0.1",
+    author="Catherine_Harvey",
+    author_email="harveycas@nih.gov",
+    description="A package to help analyze tetrads",
+    long_description= "A package to facilitate analyzing the antibiotic markers of yeast tetrads",
+    long_description_content_type="text/markdown",
+    url="https://github.com/harveycas/project_spring_2020",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+
+)
+
---
+++ tetrad_analysis/.ipynb_checkpoints/dataframe_functions-checkpoint.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import ast
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+
+
+def open_tetrad(excel):
+    """cleaning a specific document from your computer"""
+        
+    #reading the excel file that includes all of the tetrad data
+    df = pd.read_excel('python_test_list.xlsx')
+    
+    
+    
+    #delete the automatically generated column on the left-hand side
+    df_index = df.set_index(first_column)
+    df_index = df.set_index('Plate')
+
+#make sire that this index was actually created 
+print(df_index)
+
+
+def sort_NAT(excel):
+    '''This will allow for you to select for spores in tetrads that test positive for NAT and place them in their own excel document'''
+   
+    
+    #separate the positive spores from the negative spores
+    df_index.sort_values(['NAT'], ascending=False)
+    NAT_positive = df_index[df_index['NAT'] > 0]
+
+             
+        
+    #now take the positive ones and put them into their own excel document
+    dfNAT = pd.DataFrame(NAT_positive)
+    dfNAT.to_excel("NAT_positive_python_fp.xlsx")
+    
+        
+print(NAT_positive)    
+
+    
+    
+
+def sort_HYG(excel):
+    '''this will allow for you to select for spores in tetrads that test positive for HYG and place them in their own excel document'''
+
+    #separate the positive spores from the negative spores
+    sort_by_HYG = df_index.sort_values(['HYG'], ascending=False)
+    HYG_positive = df_index[df_index['HYG'] > 0]
+    
+
+    #now take only the positive ones and place them in their own excel document
+    dfNAT = pd.DataFrame(HYG_positive)
+    dfNAT.to_excel("HYG_positive_python_fp.xlsx")
+
+print(HYG_positive)
+       
+        
+
+def sort_URA(excel):
+    '''this will allow for you to select for spores in tetrads that test positiev for URA and place them in their own excel document'''
+
+    #separate the positive spores from the negative spores
+    sort_by_URA = df_index.sort_values(['URA'], ascending=False)
+    URA_positive = df_index[df_index['URA'] > 0]
+    
+
+    #now take only the positive ones and place them in their own document
+    dfNAT = pd.DataFrame(URA_positive)
+    dfNAT.to_excel("URA_positive_python_fp.xlsx")
+
+    
+print(URA_positive)    
+    
+    
+    
+    
+    
+
+def combine_antibiotics(excel):
+    '''this will take the three marker documents that you made and place them in one major excel document where each marker is one sheet of the document'''
+
+    #create one dataframe containing all of the positive markers
+    all_positive = {'NAT_plus': NAT_positive, 'HYG_plus': HYG_positive, 'URA_plus': URA_positive}
+    
+    #turn this new combined dataframe into an excel document
+    writer = pd.ExcelWriter('Antibiotic_markers.xlsx', engine='xlsxwriter')
+    
+    for sheet_name in all_positive.keys():
+        all_positive[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
+
+    writer.save()
+    
+
+print("finished")
+    
+   
+
+
