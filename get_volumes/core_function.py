5bfb141dbf2b3b2569d8a649c6bde703b9fa57b5
dferhadian <dferhadian@gmail.com>
2020-05-07 19:42:25 -0400

    Updated test

--- get_volumes/core_function.py
+++ get_volumes/core_function.py
@@ -1,7 +1,9 @@
 import pandas as pd
-data = pd.read_csv("tests/Data/Data.csv")  #if UnicodeDecodeError open csv file in Sublime Text and Save with UTF-8 encoding
-data.head()
-clean_data = data.copy()[['Sample Time','Type', 'Conc.']]
-clean_data.head()
-clean_data["Volume"] = 700/clean_data["Conc."]
-clean_data.head()
+def get_volumes(input):
+    data = pd.read_csv(input)  #if UnicodeDecodeError open csv file in Sublime Text and Save with UTF-8 encoding
+    data.head()
+    clean_data = data.copy()[['Sample Time','Type', 'Conc.']]
+    clean_data.head()
+    clean_data["Volume"] = 700/clean_data["Conc."]
+    clean_data.head()
+    return clean_data
--- tests/get_volume_test.py
+++ tests/get_volume_test.py
@@ -3,8 +3,8 @@ from get_volumes import core_function
 
 
 def test_get_volumes():
-    expected = pd.DataFrame({"Conc.":[10,10.1,12,2],'volumes':[70,69.3,57.38]})
-    filename = 'Data/Data.csv'
-    input = pd.DataFrame.read_csv(filename).head(3)
-    result = core_functions.get_volumes(input)
-    assert result == expected
+    expected = pd.DataFrame({"Conc.":[10.0,10.1,12.2],'Volume':[70,69.3,57.38]})
+    filename = 'Data.csv'
+    result = core_function.get_volumes(filename)
+    assert (result.head(3)["Conc."].round(1) == expected["Conc."].round(1)).all
+    assert (result.head(3)["Volume"].round(0) == expected["Volume"].round(0)).all
