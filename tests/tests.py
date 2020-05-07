62301a2cd7904dd0a89a4014432f1477bb1a3bc2
aob93 <60762425+aob93@users.noreply.github.com>
2020-05-07 18:48:15 -0400

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -6,10 +6,12 @@ ann_paths_file = 'peak.paths.txt'
 output = 'test_output'
 xlab = 'test_xlab'
 
-def test_read_of_locus_file():
+def test_read_of_locus_file(): # Check that locus file has been formatted properly
     header = main(locus_file, ann_paths_file, output, xlab)
     assert header[0] == 'Chr'
+    assert header[1] == 'pos'
+    assert header[2] == 'RSID'
     
 def test_read_of_ann_paths_file():
     ann = main(locus_file, ann_paths_file, output, xlab)
-    assert ann[0] == 'Chr'
+    assert ann[0] == 'Chr' #First element of header will be Chr as bedfiles are standardised format
