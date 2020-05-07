cd86334c6d7d77e0655c50fe1fe172c282e6b8b3
dferhadian <60761137+dferhadian@users.noreply.github.com>
2020-05-07 19:10:11 -0400

    Update get_volume_test.py

--- tests/get_volume_test.py
+++ tests/get_volume_test.py
@@ -4,7 +4,7 @@ from get_volumes import core_function
 
 def test_get_volumes():
     expected = pd.DataFrame({"Conc.":[10,10.1,12,2],'volumes':[70,69.3,57.38]})
-    filename = 'data/data.csv'
+    filename = 'Data/Data.csv'
     input = pd.DataFrame.read_csv(filename).head(3)
     result = core_functions.get_volumes(input)
     assert result == expected
