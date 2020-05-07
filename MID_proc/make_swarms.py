2f6285b6b2c21db400f9d8fca5553637dd3d39dc
Christina Wusinich <christinawusinich@gmail.com>
2020-05-07 03:23:01 -0400

    Added a second functional test; need to figure out how to supress extraneous printing tho

--- MID_proc/make_swarms.py
+++ MID_proc/make_swarms.py
@@ -8,14 +8,14 @@ from datetime import datetime
 
 #set directories we're going to use
 subdir='/data/MoodGroup/07M0021_meg_analysis/MID_data/subjects/'
-swarmdir='/data/MoodGroup/07M0021_meg_analysis/MID_data/scripts/swarm/'
+default_swarmdir='/data/MoodGroup/07M0021_meg_analysis/MID_data/scripts/swarm/'
 
 #set current date for use in filenames
 date_today=datetime.now().strftime('%m%d%Y')
 
 #set default file with list of subjects we want included
 rootdir='/data/MoodGroup/07M0021_meg_analysis/MID_data/'
-default_sublist=swarmdir +'processinglist.txt'
+default_sublist=default_swarmdir +'processinglist.txt'
 
 #set default variables here:
 default_origds='_MID-f.ds'
@@ -28,7 +28,7 @@ default_freqband='highgamma'
 
 
 ### MAKE NEWDS SWARM FILE
-def make_swarm_newDs(subjectlist=default_sublist, origds=default_origds, newds=default_newds, marker=default_marker, timewindow=default_timewindow):
+def make_swarm_newDs(subjectlist=default_sublist, origds=default_origds, newds=default_newds, marker=default_marker, timewindow=default_timewindow,swarmdir=default_swarmdir):
 	"""Makes swarm file for creating new MEG datasets based on input parameters."""
 	#create newDs swarm file and add info about how to run it
 	swarmfile_name=f'{swarmdir}newDs_{date_today}.swarm'
@@ -54,7 +54,7 @@ def make_swarm_newDs(subjectlist=default_sublist, origds=default_origds, newds=d
 
 
 ### MAKE SAM SWARM FILES
-def make_swarm_sam(subjectlist=default_sublist, ds=default_ds, marker=default_marker, freqband=default_freqband):
+def make_swarm_sam(subjectlist=default_sublist, ds=default_ds, marker=default_marker, freqband=default_freqband,swarmdir=default_swarmdir):
 	#read list of subjects	
 	sublist=open(subjectlist, 'r')
 	proclist=sublist.read().splitlines()
