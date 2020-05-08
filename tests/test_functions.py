2a7f03ecd8ab8f07ecf5df63b4943e0a1d1ec981
leej3 <johnleenimh@gmail.com>
2020-05-07 20:52:54 -0400

    demo improved tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,13 @@
 from project_code.annotation_plot import main
+from pathlib import Path
+
+tests_dir = Path(__file__).parent
+
 
 # read in test data
-locus_file = 'chr5.cs.snps.txt'
-ann_paths_file = 'peak.paths.txt'
-output = 'test_output'
+locus_file = tests_dir / 'chr5.cs.snps.txt'
+ann_paths_file = tests_dir /  'peak.paths.txt'
+output =   'test_output'
 xlab = 'test_xlab'
 
 def test_read_of_locus_file(): # Check that locus file has been formatted properly
