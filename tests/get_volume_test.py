c588ceb76b82eac00db1ba3d21166273b510c06c
leej3 <johnleenimh@gmail.com>
2020-05-07 23:11:01 -0400

    fix data path
    __file__ is a variable for the file that python is currently executing
    useful in this case because the current working can be different
    depending on where you ran your tests from

--- tests/get_volume_test.py
+++ tests/get_volume_test.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from get_volumes import core_function
+from pathlib import Path
 
-
+tests_dir = Path(__file__).parent
+DATAPATH = tests_dir / 'Data.csv'
 def test_get_volumes():
     expected = pd.DataFrame({"Conc.":[10.0,10.1,12.2],'Volume':[70,69.3,57.38]})
-    filename = 'Data.csv'
-    result = core_function.get_volumes(filename)
+    
+    result = core_function.get_volumes(DATAPATH)
     assert (result.head(3)["Conc."].round(1) == expected["Conc."].round(1)).all
     assert (result.head(3)["Volume"].round(0) == expected["Volume"].round(0)).all
