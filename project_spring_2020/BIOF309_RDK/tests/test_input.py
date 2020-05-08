98bafa571076ed5f8948eb39fc93b1b905d9212b
leej3 <johnleenimh@gmail.com>
2020-05-08 12:10:55 -0400

    some little fixes

--- project_spring_2020/BIOF309_RDK/tests/test_input.py
+++ project_spring_2020/BIOF309_RDK/tests/test_input.py
@@ -1,14 +1,17 @@
 
 from EHT_RDK.input_files import *
+from pathlib import Path
+
+tests_dir = Path(__file__).parent
 
 def test_input_list():
-    directory_name = "/Users/dewanr2/Documents/Ramitas_Docs/NIH_Classes/BIOF309/Input_test"
+    directory_name = tests_dir
     obs = create_input_list(directory_name)
     exp = []
     assert obs == exp
-    
+
 def test_df_list():
-    dir_name = "/Users/dewanr2/Documents/Ramitas_Docs/NIH_Classes/BIOF309/create_df_test"
+    dir_name = tests_dir / "create_df_test"
     file_list = ['AFF2.txt']
     obs = create_dataframe_list(file_list, dir_name)
     exp = NotImplemented
